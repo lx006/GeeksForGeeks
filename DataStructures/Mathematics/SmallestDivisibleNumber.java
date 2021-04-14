c01f14d1fafafc097c326a56c96bd34f894acb5d
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-04-14 19:08:39 +0530

    Update SmallestDivisibleNumber.java

--- DataStructures/Mathematics/SmallestDivisibleNumber.java
+++ DataStructures/Mathematics/SmallestDivisibleNumber.java
@@ -1,1 +1,28 @@
+class Solution
+{
+    public static long getSmallestDivNum(int n)
+    {
+        long gcd = 1;
+        long lcm = 1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            long mul = gcd(lcm,i);
+            lcm = lcm*i/mul;
+        }
+        return lcm;
+    }
+    
+    public static long gcd(long a, long b)
+    {
+        if(a==0)
+        {
+            return b;
+        }
+        else
+        {
+            return gcd(b%a,a);
+        }
+    }
+}
 
